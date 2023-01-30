2fffb611708cff48de97aea5649804f4811c7dc0
Victor-Nyangi <gichuivictor@gmail.com>
2023-01-30 12:51:06 +0300

    feat:Bug fix

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(process.env.REACT_APP_CLIENT_ID)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
