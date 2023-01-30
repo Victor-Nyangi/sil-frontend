063a9acb1516e2a0a94aed0b86c4d0afa0fb9cb3
Victor-Nyangi <gichuivictor@gmail.com>
2023-01-30 12:25:58 +0300

    feat:Modifying the Photo test

--- src/__tests__/Photo.test.js
+++ src/__tests__/Photo.test.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Photo } from '../views'
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
+
+jest.mock("axios");
+
+const dummyPhoto = {
+albumId: 8,
+id: 353,
+title: "quia consequatur fugit atque est saepe",
+url: "https://via.placeholder.com/600/44e038",
+thumbnailUrl: "https://via.placeholder.com/150/44e038",
+photo_id: 353
+}
 describe('The Photo page', () => {
     test("image renders", async () => {
+        axios.get.mockResolvedValue({ data: dummyPhoto });
 
         render(<GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
             <Photo />
         </GoogleOAuthProvider>, { wrapper: BrowserRouter }); // Test if the page renders accurately before any API calls or selections are made
 
-        const photo = screen.getAllByRole("img");
+        const photo = await waitFor(() => screen.findAllByRole("img"));
         expect(photo).toHaveLength(1);
-    })
-
-    // Search button
-    test("Search Button renders", () => {
-        render(<GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-            <Photo />
-        </GoogleOAuthProvider>, { wrapper: BrowserRouter });
-        const searchBtn = screen.getByRole("button");
-        expect(searchBtn.textContent).toBe("Update");
-        expect(searchBtn).not.toBeDisabled();
     })
 });
